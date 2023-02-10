d947eb80c3a80a4cdb3f5b2f5e8808d51e9cd572
Cream <marstarty3631@gmail.com>
2023-02-11 01:28:02 +0800

    message

---
+++ src/views/AboutView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default{
+  data(){
+    return{
+      data:{}
+    }
+  },
+  mounted(){
+    console.log(import.meta.env.VITE_TEXT)
+    console.log(import.meta.env.VITE_PATH)
+    this.$http.get('https://randomuser.me/api/')
+      .then((res)=>{
+        console.log(res);
+        this.data=res.data.results[0];
+        console.log(this.data);
+      })
+  }
+}
+</script>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
